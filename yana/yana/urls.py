cc6490357c5f739cea0bd4daa2b474cce12d1583
maksfrol96 <maxfrol96@mail.ru>
2023-02-05 19:25:30 +0300

    Создание приложения

--- yana/yana/urls.py
+++ yana/yana/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from yana_site
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
 ]
--- yana/yana_site/apps.py
+++ yana/yana_site/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SiteYanaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_yana'
+    name = 'yana_site'
